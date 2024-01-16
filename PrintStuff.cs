dd6400cf0beb86da0a55d6853e5ed588ef84ef25
mikaylarandall <mikaylarandall@MIKAYLARANDC42A>
2024-01-16 14:09:01 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreC_Fun
+{
+    internal class PrintStuff
+    {
+        // Constructor
+        private string language;
+
+        public PrintStuff ()
+        {
+            language = "EN";
+        }
+
+        public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+
+        public void PrintName(string fn)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + fn + "!");
+            }
+            if (language == "PT")
+            {
+                System.Console.WriteLine("OI, " + fn + "!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using moreC_Fun;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff();
+
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+    }
+}
